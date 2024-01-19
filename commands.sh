424b820b02e7231d632d69aec91295b435b01f97
root <daveey@gmail.com>
2024-01-18 17:08:08 -0800

    fix command

--- commands.sh
+++ commands.sh
@@ -5,7 +5,8 @@ aws s3 sync s3://metta-ai/vast/workspace/metta/train_dir ./train_dir
 python -m envs.griddly.gridman.train \
     --algo=APPO \
     --env=GDY-GridmanMultiAgent \
-    --experiment=gm.rnn=1024 \
-    --with_wandb \
+    --experiment=gm.rnn=2048 \
+    --rnn_size=2048 \
+    --with_wandb=True \
     --num_workers=25 \
 
