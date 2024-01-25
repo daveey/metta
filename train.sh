c384575d40e425d6b78d7c149772fce164db7e73
David Bloomin <daveey@gmail.com>
2024-01-24 16:55:59 -0800

    train.sh

--- train.sh
+++ train.sh
@@ -7,5 +7,12 @@ python -m envs.griddly.train \
     --num_workers=25 \
     --decorrelate_experience_max_seconds=100 \
     --lr_schedule=kl_adaptive_epoch \
+    --forage.num_agents=10 \
+    --forage.width_min=10 \
+    --forage.width_max=100 \
+    --forage.height_min=10 \
+    --forage.height_max=100 \
+    --forage.energy_per_agent=2 \
+    --forage.wall_density=0.1 \
     "$@"
 
