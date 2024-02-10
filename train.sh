fcc08137ba3d767981e61cca856ad512d871b194
David Bloomin <daveey@gmail.com>
2024-02-10 00:35:41 -0800

    update train script?

--- train.sh
+++ train.sh
@@ -6,9 +6,10 @@ python -m envs.griddly.train \
     --with_wandb=True \
     --num_workers=25 \
     --decorrelate_experience_max_seconds=100 \
-    --env_num_agents=10 \
-    --orb_world_width=10:100 \
-    --orb_world_height=10:100 \
-    --orb_world_energy_per_agent=1 \
+    --env_num_agents=50 \
+    --orb_world_width=30:200 \
+    --orb_world_height=30:200 \
     --orb_world_wall_density=0.1 \
+    --orb_world_factories_per_agent=1:10 \
+    --orb_world_initial_energy=50 \
     "$@"
