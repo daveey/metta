0d2820862c3c4c9c9004b3b7d2058d37d35a550b
David Bloomin <daveey@gmail.com>
2024-02-19 11:16:46 -0800

    change to power_grid

---
+++ envs/griddly/power_grid/power_grid_env.py
@@ -0,0 +1,95 @@
+import argparse
+import enum
+import stat
+
+import gymnasium as gym
+import jmespath
+import numpy as np
+import yaml
+from griddly.gym import GymWrapper
+from gymnasium.core import Env
+from envs.griddly.power_grid.power_grid_level_generator import PowerGridLevelGenerator
+
+import util.args_parsing as args_parsing
+import cv2
+import matplotlib.pyplot as plt
+GYM_ENV_NAME = "GDY-PowerGrid"
+
+class PowerGridEnvWrapper(gym.Wrapper):
+    def __init__(self, env: Env, level_generator: PowerGridLevelGenerator):
+        super().__init__(env)
+        self.level_generator = level_generator
+
+        obj_order = self.env.game.get_object_names()
+        var_order = self.env.game.get_object_variable_names()
+        assert obj_order == sorted(obj_order)
+        assert var_order == sorted(var_order)
+
+        # self.obs_order = list(range(len(obj_order) + len(var_order)))
+        # num_objects = len(obj_order)
+
+        # for expected, obj in enumerate(sorted(obj_order)):
+        #     self.obs_order[expected] = obj_order.index(obj)
+        # for expected, var in enumerate(sorted(var_order)):
+        #     self.obs_order[num_objects + expected] = num_objects + var_order.index(var)
+
+    def reset(self, **kwargs):
+        kwargs = kwargs or {}
+        kwargs["options"] = {"level_string": self.level_generator.make_level_string()}
+        obs, infos = self.env.reset(**kwargs)
+        return self._reorder_obs(obs), infos
+
+    def step(self, actions):
+        obs, rewards, terminated, truncated, infos = self.env.step(actions)
+        if terminated or truncated:
+            stat_names = list(filter(
+                lambda x: x.startswith("stats:"),
+                self.env.game.get_global_variable_names()))
+            stats = self.env.game.get_global_variable(stat_names)
+            for agent in range(self.env.player_count):
+                infos["episode_extra_stats"] = [{}] * self.env.player_count
+                for stat_name in stat_names:
+                    # some are per-agent, some are just global {0: val}
+                    stat_val = stats[stat_name][0]
+                    if len(stats[stat_name]) > 1:
+                        stat_val = stats[stat_name][agent + 1]
+                    infos["episode_extra_stats"][agent][stat_name] = stat_val
+        rewards = np.array(rewards) / 10.0
+        return self._reorder_obs(obs), rewards, terminated, truncated, infos
+
+    def _reorder_obs(self, obs):
+        return obs
+        # return [ob[self.obs_order] for ob in obs]
+
+    @staticmethod
+    def make_env(cfg, level_generator, render_mode="rgb_array"):
+        """
+        Creates a new instance of the PowerGrid environment.
+
+        Returns:
+            An instance of the PowerGrid environment.
+        """
+        with open("./envs/griddly/power_grid/gdy/power_grid.yaml", encoding="utf-8") as file:
+            game_config = yaml.safe_load(file)
+
+        game_config["Environment"]["Player"]["Count"] = cfg.env_num_agents
+        game_config["Environment"]["Levels"] = [level_generator.make_level_string()]
+        init_energy = level_generator.sample_initial_energy()
+        _update_global_variable(game_config, "conf:agent:initial_energy", init_energy)
+
+        env = PowerGridEnvWrapper(GymWrapper(
+                yaml_string=yaml.dump(game_config),
+                player_observer_type="VectorAgent",
+                global_observer_type="GlobalSpriteObserver",
+                level=0,
+                max_steps=cfg.env_max_steps,
+                render_mode=render_mode,
+            ), level_generator)
+        return env
+
+def _update_global_variable(game_config, var_name, value):
+    jmespath.search('Environment.Variables[?Name==`{}`][]'.format(var_name), game_config)[0]["InitialValue"] = value
+
+def _update_object_variable(game_config, object_name, var_name, value):
+    jmespath.search('Objects[?Name==`{}`][].Variables[?Name==`{}`][]'.format(
+        object_name, var_name), game_config)[0]["InitialValue"] = value
---
+++ envs/griddly/power_grid/power_grid_level_generator.py
@@ -0,0 +1,128 @@
+import argparse
+
+import gymnasium as gym
+import numpy as np
+import yaml
+from griddly.gym import GymWrapper
+from gymnasium.core import Env
+
+import util.args_parsing as args_parsing
+
+class PowerGridLevelGenerator():
+
+    def __init__(self, cfg=None):
+        """
+        Args:
+            cfg: Optional configuration object.
+        """
+        self.cfg = cfg
+        self.num_agents = self.cfg.env_num_agents
+
+        self.sample_level_width = args_parsing.get_value_possibly_from_range(self.cfg.power_grid_width, np.random.randint)
+        self.sample_level_height = args_parsing.get_value_possibly_from_range(self.cfg.power_grid_height, np.random.randint)
+        self.sample_level_wall_density = args_parsing.get_value_possibly_from_range(self.cfg.power_grid_wall_density, np.random.uniform)
+        self.sample_num_chargers_per_agent = args_parsing.get_value_possibly_from_range(self.cfg.power_grid_chargers_per_agent, np.random.uniform)
+        self.sample_initial_energy = args_parsing.get_value_possibly_from_range(self.cfg.power_grid_initial_energy, np.random.uniform)
+
+    def make_level_string(self):
+        """
+        Generates a string representation of the level configuration.
+
+        Returns:
+            A string representation of the level configuration.
+        """
+        return "\n".join(["  ".join(row) for row in self._make_level()])
+
+    def _make_level(self):
+        """
+        Generates the level configuration.
+
+        Returns:
+            A 2D list representing the level configuration.
+        """
+        width = self.sample_level_width()
+        height = self.sample_level_height()
+        chargers = int(self.num_agents * self.sample_num_chargers_per_agent())
+
+        # make the bounding box
+        level = [["."] * width for _ in range(height)]
+        level[0] = ["W"] * width
+        level[-1] = ["W"] * width
+        for i in range(height):
+            level[i][0] = "W"
+            level[i][-1] = "W"
+
+        # make the agents
+        for i in range(self.num_agents):
+            # level[4][2*i] = f"A{i+1}"
+            while True:
+                x = np.random.randint(1, width-1)
+                y = np.random.randint(1, height-1)
+                if level[y][x] == ".":
+                    level[y][x] = f"A{i+1}"
+                    break
+
+        # make the energy
+        for i in range(chargers):
+            # level[40][2*i] = f"o"
+            for _ in range(10):
+                x = np.random.randint(1, width-1)
+                y = np.random.randint(1, height-1)
+                if level[y][x] == ".":
+                    level[y][x] = "o"
+                    break
+
+        # make obstacles
+        wall_density = self.sample_level_wall_density()
+
+        for i in range(int(width*height*wall_density)):
+            x = np.random.randint(1, width-1)
+            y = np.random.randint(1, height-1)
+            if level[y][x] == ".":
+                level[y][x] = "W"
+        return level
+
+
+def add_env_args(parser: argparse.ArgumentParser) -> None:
+    """
+    Add environment-specific arguments to the argument parser.
+
+    Args:
+        parser (argparse.ArgumentParser): The argument parser object.
+
+    Returns:
+        None
+    """
+    p = parser
+
+
+    p.add_argument("--power_grid_width",
+                   default=[15],
+                   help='Level width. Can be either a integer value OR a low:high range (e.g., "10:20" to sample from [10, 20) - high excluded)',
+                   action=args_parsing.PossiblyNumericRange2Number,
+                   str2numeric_cast_fn=int)
+
+    p.add_argument("--power_grid_height",
+                   default=[15],
+                   help='Level height. Can be either a single integer OR a low:high range (e.g., "10:20" to sample from [10, 20) - high excluded)',
+                   action=args_parsing.PossiblyNumericRange2Number,
+                   str2numeric_cast_fn=int)
+
+    p.add_argument("--power_grid_chargers_per_agent",
+                   default=[1],
+                   help='Chargers per agent.',
+                   action=args_parsing.PossiblyNumericRange2Number,
+                   str2numeric_cast_fn=float)
+
+    p.add_argument("--power_grid_initial_energy",
+                   default=[100],
+                   help='Initial enrergy.',
+                   action=args_parsing.PossiblyNumericRange2Number,
+                   str2numeric_cast_fn=int)
+
+    p.add_argument("--power_grid_wall_density",
+                   default=[0.05],
+                   help='Level wall density. Can be either a single float OR a low:high range (e.g., "0.2:0.5") from which a value is uniformly drawn',
+                   action=args_parsing.PossiblyNumericRange2Number,
+                   str2numeric_cast_fn=float)
+
