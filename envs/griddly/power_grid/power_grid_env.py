738f35867b482d024d23e41871f8265f1d3d81d4
David Bloomin <daveey@gmail.com>
2024-02-28 23:02:42 -0800

    update train params

--- envs/griddly/power_grid/power_grid_env.py
+++ envs/griddly/power_grid/power_grid_env.py
@@ -110,8 +110,8 @@ class PowerGridEnv(gym.Env):
                 stat_val = stats[stat_name][0]
                 if len(stats[stat_name]) > 1:
                     stat_val = stats[stat_name][agent + 1]
-                if stat_name.startswith("stats:action"):
-                    stats_val /= self._max_steps
+                    if stat_name.startswith("stats:action"):
+                        stats_val /= self._max_steps
                 infos["episode_extra_stats"][agent][stat_name] = stat_val / self.NORMALIZATION.get(stat_name, 1)
             infos["episode_extra_stats"][agent]["level_max_energy"] = self._max_level_energy
             infos["episode_extra_stats"][agent]["level_max_energy_per_agent"] = self._max_level_energy / self._griddly_env.player_count
