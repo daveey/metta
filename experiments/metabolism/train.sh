98882300af4f9884d2cd6649c8c07ddc31582eb0
David Bloomin <daveey@gmail.com>
2024-03-04 14:45:23 -0800

    cleanup experiments

---
+++ experiments/metabolism/train.sh
@@ -0,0 +1,13 @@
+#/bin/bash -e
+
+./experiments/train.sh \
+    --env_width=10 \
+    --env_height=10 \
+    --env_num_altars=1 \
+    --env_num_chargers=1 \
+    --env_num_generators=10 \
+    --env_wall_density=0 \
+    --env_cost:shield=10000 \
+    --env_cost:attack=10000 \
+    --env_agent:initial_energy=0 \
+    "$@"
