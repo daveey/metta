7e58b2d816cb8a7af6e82b769adb090690b8be37
David Bloomin <daveey@gmail.com>
2024-03-05 22:26:14 -0800

    energy rewards

--- train_met.sh
+++ train_met.sh
@@ -10,7 +10,7 @@
     --env_wall_density=0 \
     --env_reward_rank_steps=1000 \
     --env_reward:use=1 \
-    --env_reward:metabolism=1 \
+    --env_reward:metabolism=0 \
     --env_altar:reward=100 \
     --env_altar:cost=100 \
     --env_reward_prestige_weight=0 \
