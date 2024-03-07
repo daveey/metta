aaff17349ea8fe786df7ef2592ee473cf2b05417
David Bloomin <daveey@gmail.com>
2024-03-06 17:46:32 -0800

    cp

--- evaluate.sh
+++ evaluate.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -e
 
-python -m envs.griddly.enjoy \
+python -m enjoy \
     --seed=0 \
     --env=GDY-PowerGrid \
     --device=cpu \
     --train_dir=./train_dir/ \
     --fps=0 \
-    --no_render \
     --max_num_frames=1000 \
     --eval_env_frameskip=1 \
     --load_checkpoint_kind=best \
-    --experiment "$@"
+   "$@"
 
