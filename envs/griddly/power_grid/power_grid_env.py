ac062297836823801d1af8e208a412ce363c5c9d
David Bloomin <daveey@gmail.com>
2024-03-18 08:59:24 -0700

    add action padding

--- envs/griddly/power_grid/power_grid_env.py
+++ envs/griddly/power_grid/power_grid_env.py
@@ -24,7 +24,7 @@ class PowerGridEnv(gym.Env):
     OBS_NUM_FEATURES = 23 # 35
     OBS_GLOBAL_VARS = 20 # 100
     OBS_KINSHIP_FEATURES = 1 # 10
-    NUM_ACTIONS = 7 #20
+    NUM_ACTIONS = 20
 
     def __init__(self, level_generator: PowerGridLevelGenerator, render_mode="rgb_array"):
         super().__init__()
