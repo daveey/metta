141c50eb3f64cf918cebf42983ed195d9dc617a9
David Bloomin <daveey@gmail.com>
2024-03-19 12:46:22 -0700

    update training args

--- train.sh
+++ train.sh
@@ -16,10 +16,14 @@ python -m train \
     --env_num_generators=5:20 \
     --env_wall_density=0:0.15 \
     --env_reward_rank_steps=10 \
-    --agent_fc_layers=10 \
+    --agent_fc_layers=3 \
     --agent_fc_size=512 \
+    --agent_embedding_size=64 \
+    --agent_embedding_layers=3 \
+    --agent_attention_size=512 \
+    --agent_attention_layers=2 \
     --rnn_num_layers=2 \
-    --rnn_size=256 \
+    --rnn_size=512 \
     --rnn_type=gru \
     --rollout=256 \
     --batch_size=65536 \
