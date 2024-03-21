676adadc4731b4dcba4ae52e2f9558fe7794aa17
David Bloomin <daveey@gmail.com>
2024-03-21 13:29:06 -0700

    fix python setup

--- setup.py
+++ setup.py
@@ -18,18 +18,15 @@ class BuildGriddlyCommand(Command):
              '--profile', 'deps/build.profile', '-s', 'build_type=Release',
              '--build', 'missing', '-if', 'build'],
             cwd='third_party/griddly',
-            shell=True,
         )
         subprocess.check_call(
             ['cmake', '.', '-B', 'build', '-GNinja', '-DCMAKE_BUILD_TYPE=Release',
              '-DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake'],
             cwd='third_party/griddly',
-            shell=True,
         )
         subprocess.check_call(
             ['cmake', '--build', 'build', '--config', 'Release'],
             cwd='third_party/griddly',
-            shell=True,
         )
 
     def initialize_options(self):
