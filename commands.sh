8c3f0bb0082d96ab60a544a5b7543b3a20518905
David Bloomin <daveey@gmail.com>
2024-03-21 11:46:49 -0700

    cleanup repo

--- commands.sh
+++
@@ -1,8 +0,0 @@
-# copy to s3
-aws s3 ./train_dir sync s3://metta-ai/vast/workspace/metta/train_dir
-
-# copy models from s3
-aws s3 sync s3://metta-ai/vast/workspace/metta/train_dir ./train_dir
-
-# train
-
