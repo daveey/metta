7864b034e8faa45a8cef257cdd0b4d3b2b2549f3
David Bloomin <daveey@gmail.com>
2024-06-05 17:49:28 -0700

    normalize rewards

--- env/griddly/mettagrid/gym_env.py
+++ env/griddly/mettagrid/gym_env.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 
 import gymnasium as gym
 import hydra
+import numpy as np
 from omegaconf import OmegaConf
 from env.griddly.griddly_gym_env import GriddlyGymEnv
 from env.griddly.mettagrid.game_builder import MettaGridGameBuilder
@@ -42,45 +43,40 @@ class MettaGridGymEnv(gym.Env):
     def step(self, actions):
         obs, rewards, terminated, truncated, info = self._env.step(actions)
 
+        rewards = np.array(rewards) / self._max_level_reward_per_agent
+
         if terminated or truncated:
             self.process_episode_stats(info["episode_extra_stats"])
 
-        return obs, rewards, terminated, truncated, info
+        return obs, list(rewards), terminated, truncated, info
 
     def process_episode_stats(self, episode_stats: Dict[str, Any]):
-        max_energy = self._compute_max_energy()
-        max_energy_per_agent = max_energy / self.player_count
         for agent_stats in episode_stats:
             for stat_name in agent_stats.keys():
                 if stat_name.startswith("stats_action_"):
                     agent_stats[stat_name] /= self._griddly_env.num_steps
 
-            agent_stats["level_max_energy"] = max_energy
-            agent_stats["level_max_energy_per_agent"] = max_energy_per_agent
-            agent_stats["level_max_reward_per_agent"] = max_energy_per_agent * 3
+            agent_stats["level_max_energy"] = self._max_level_energy
+            agent_stats["level_max_energy_per_agent"] = self._max_level_energy_per_agent
+            agent_stats["level_max_reward_per_agent"] = self._max_level_reward_per_agent
 
     def _compute_max_energy(self):
-        return 1
-        # # compute the max possible energy for the level
-        # charger_energy, generator_cooldown, agent_init, agent_regen = map(
-        #     lambda x: float(x[0]),
-        #     self._griddly_env.unwrapped.get_global_variables([
-        #         "conf:charger:energy",
-        #         "conf:generator:cooldown",
-        #         "conf:agent:energy:initial",
-        #     ).values())
+        max_resources = self._game_builder.object_configs.generator.count * min(
+            self._game_builder.object_configs.generator.initial_resources,
+            self._max_steps / self._game_builder.object_configs.generator.cooldown)
+        max_conversions = self._game_builder.object_configs.converter.count * (
+            self._max_steps / self._game_builder.object_configs.converter.cooldown
+        )
+        max_conv_energy = min(max_resources, max_conversions) * \
+            self._game_builder.object_configs.converter.energy_output
 
-        # num_steps = self._max_steps
-        # num_generators = len(list(
-        #     filter(lambda x: x["Name"] == "generator",
-        #     self._griddly_env.game.get_state()["Objects"])))
-        # max_resources = num_generators * (1 + num_steps // generator_cooldown)
-        # max_charger_energy = float(charger_energy) * max_resources
-        # max_agent_energy = float(agent_init) + agent_regen * num_steps
-        # self._max_level_energy = max_charger_energy + self._num_agents * max_agent_energy
-        # self._max_level_energy_per_agent = self._max_level_energy / self._num_agents
-        # # if the agents use all their energy for the altar, we get a 3x reward
-        # self._max_level_reward_per_agent = self._max_level_energy_per_agent * 3
+        initial_energy = self._game_builder.object_configs.agent.initial_energy * self._game_builder.num_agents
+
+        self._max_level_energy = max_conv_energy + initial_energy
+        self._max_level_energy_per_agent = self._max_level_energy / self._game_builder.num_agents
+
+        # if the agents use all their energy for the altar, we get a 3x reward
+        self._max_level_reward_per_agent = self._max_level_energy_per_agent * 3
 
 
     @property
