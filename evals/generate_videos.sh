248b784cb77596d8d34f9a9b8df3019c2ef9c464
David Bloomin <daveey@gmail.com>
2024-06-20 11:45:24 -0700

    add pufferlib

--- evals/generate_videos.sh
+++ evals/generate_videos.sh
@@ -3,12 +3,16 @@
 experiment=$1
 shift
 
-for envcfg in a5_25x25 a20_40x40 a20_r4_40x40 a20_b4_40x40; do
+behavior_cfgs=$(find configs/env/mettagrid/behaviors/ -name "*.yaml" | sed 's|.*/behaviors/\(.*\)\.yaml|behaviors\1|')
+train_cfgs="a5_25x25 a20_40x40 a20_r4_40x40 a20_b4_40x40"
+
+for envcfg in $behavior_cfgs  $train_cfgs ; do
     echo "Generating video for $envcfg"
+    video_name=${envcfg//\//_}
     python -m tools.evaluate \
         env=mettagrid/$envcfg \
         sample_factory=video \
-        +sample_factory.video_name="${envcfg}.mp4" \
+        +sample_factory.video_name="${video_name}.mp4" \
         +sample_factory.experiment=$experiment \
         "$@"
 done
